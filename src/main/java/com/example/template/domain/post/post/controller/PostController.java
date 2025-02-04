fa482b00fbf410267b1dc8e73b80575c03a82708
joseph <wdg0434@gmail.com>
2025-02-04 16:47:49 +0900

    feat#4: doWrite

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -7,21 +7,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/posts")
 public class PostController {
-    @GetMapping("/")
-    @ResponseBody
-    public String hello() {
-        return "hello";
-    }
-
     @GetMapping("/write")
     @ResponseBody
     public String showWrite() {
         return """
-                <form action="">
+                <form action="/posts/doWrite">
                   <input type="text" name="title" placeholder="제목" />
                   <textarea name="content"></textarea>
                   <input type="submit" value="등록" />
                 </form>
                 """;
     }
+
+    @GetMapping("/doWrite")
+    @ResponseBody
+    public String doWrite(String title, String content) {
+
+        return """
+                <h1>게시물 조회</h1>
+                <div>%s</div>
+                <div>%s</div>
+                """.formatted(title, content);
+    }
+
 }
