18b92e65fa1d7df549db6f2aa0845ad82bc7a425
joseph <wdg0434@gmail.com>
2025-02-04 15:10:26 +0900

    feat#3:showWrite

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,12 +1,27 @@
 package com.example.template.domain.post.post.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
+@RequestMapping("/posts")
 public class PostController {
     @GetMapping("/")
     @ResponseBody
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/write")
+    @ResponseBody
+    public String showWrite() {
+        return """
+                <form action="">
+                  <input type="text" name="title" placeholder="제목" />
+                  <textarea name="content"></textarea>
+                  <input type="submit" value="등록" />
+                </form>
+                """;
     }
 }
