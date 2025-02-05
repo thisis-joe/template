cd1932a713b6d78bab2734262c343d79387a0350
joseph <wdg0434@gmail.com>
2025-02-05 11:17:21 +0900

    feat#11: modelAttribute apply

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,12 +1,17 @@
 package com.example.template.domain.post.post.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
 @Controller
 @RequestMapping("/posts")
+@Validated
 public class PostController {
     @GetMapping("/write")
     @ResponseBody
@@ -14,33 +19,24 @@ public class PostController {
         return getFormHtml("");
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class WriteForm {
+        @NotBlank(message = "제목을 입력해주세요.") @Length(min=5, message = "제목은 5글자 이상입니다.")
+        private String title;
+        @NotBlank(message = "내용을 입력해주세요.") @Length(min=10, message = "내용은 10글자 이상입니다.")
+        private String content;
+    }
     @PostMapping("/write")
     @ResponseBody
-    public String doWrite(String title, String content) {
-
-        if(title.isBlank() || title == null) {
-            return getFormHtml("제목을 입력해주세요.");
-        }
-
-        if(content.isBlank() || content == null) {
-            return getFormHtml("내용을 입력해주세요.");
-        }
-
-        if (title.length() < 5) {
-            return getFormHtml("제목은 5글자 이상.");
-        }
-
-        if (content.length() < 10) {
-            return getFormHtml("내용은 10글자 이상.");
-        }
-
-
+    public String doWrite(@ModelAttribute @Valid WriteForm form) {
         return """
                 <h1>게시물 조회</h1>
                 <div>%s</div>
                 <div>%s</div>
-                """.formatted(title, content);
+                """.formatted(form.title, form.content);
     }
+
     private String getFormHtml(String errorMsg) {
         return """
                     <div>%s</div>
