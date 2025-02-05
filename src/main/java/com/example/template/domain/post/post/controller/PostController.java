2193713db050153f955ab34d8ac47ccba1bf72f4
joseph <wdg0434@gmail.com>
2025-02-05 10:15:40 +0900

    feat#8#9:getFormHtml, validate upgrade

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -11,13 +11,7 @@ public class PostController {
     @GetMapping("/write")
     @ResponseBody
     public String showWrite() {
-        return """
-                <form method="post">
-                  <input type="text" name="title" placeholder="제목" />
-                  <textarea name="content"></textarea>
-                  <input type="submit" value="등록" />
-                </form>
-                """;
+        return getFormHtml("");
     }
 
     @PostMapping("/write")
@@ -25,18 +19,37 @@ public class PostController {
     public String doWrite(String title, String content) {
 
         if(title.isBlank() || title == null) {
-            return "제목을 입력해주세요";
+            return getFormHtml("제목을 입력해주세요.");
         }
 
         if(content.isBlank() || content == null) {
-            return "내용을 입력해주세요";
+            return getFormHtml("내용을 입력해주세요.");
         }
+
+        if (title.length() < 5) {
+            return getFormHtml("제목은 5글자 이상.");
+        }
+
+        if (content.length() < 10) {
+            return getFormHtml("내용은 10글자 이상.");
+        }
+
 
         return """
                 <h1>게시물 조회</h1>
                 <div>%s</div>
                 <div>%s</div>
                 """.formatted(title, content);
+    }
+    private String getFormHtml(String errorMsg) {
+        return """
+                    <div>%s</div>
+                    <form method="post">
+                      <input type="text" name="title" placeholder="제목" /> <br>
+                      <textarea name="content"></textarea> <br>
+                      <input type="submit" value="등록" /> <br>
+                    </form>
+                    """.formatted(errorMsg);
     }
 
 }
